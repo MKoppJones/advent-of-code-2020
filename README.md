09d6170cbc4d020c710db2f4cac36ce55f21319a
Michael Kopp-Jones <m.koppjones@gmail.com>
2020-12-03 08:32:33 +0000

    Update readme and run all script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 1. [Day 01](day-01) - Report Repair
 2. [Day 02](day-02) - Password Philosophy
-3. [Day 03](day-03) - ...
+3. [Day 03](day-03) - Toboggan Trajectory
 
 ## Run 'em all
 
--- run.sh
+++ run.sh
@@ -5,3 +5,7 @@ go run day-01/expenses.go day-01/expenses.txt
 
 echo ~~~~~~~~~~~~~~~~~~~
 go run day-02/passwords.go day-02/passwords.txt
+
+echo ~~~~~~~~~~~~~~~~~~~
+go run day-03/toboggan.go day-03/treeMap.txt
+
