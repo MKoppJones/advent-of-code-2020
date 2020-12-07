dcd77d06bfdf9b7cd150491afc194ecc7b5be441
Michael Kopp-Jones <m.koppjones@gmail.com>
2020-12-07 07:54:38 +0000

    Complete Day 7 Part 1

--- day-07/haversacks.go
+++ day-07/haversacks.go
@@ -4,8 +4,14 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type BagCount struct {
+	count int
+	bag   string
+}
 
 func readInput(path string) ([]string, error) {
 	bytesRead, readFileErr := ioutil.ReadFile(path)
@@ -28,12 +34,24 @@ func contains(arr []string, str string) bool {
 	return false
 }
 
-func crawlBags(bagMap map[string][]string, bag string, validBags map[string]int) map[string]int {
+func crawlCanHold(bagMap map[string][]string, bag string, validBags map[string]int) map[string]int {
 	for key, bags := range bagMap {
 		if contains(bags, bag) {
 			validBags[key] = 0
-			validBags = crawlBags(bagMap, key, validBags)
+			validBags = crawlCanHold(bagMap, key, validBags)
 		}
+	}
+	return validBags
+}
+
+func crawlHolds(bagMap map[string][]BagCount, bag string, validBags map[BagCount]int) map[BagCount]int {
+	for _, bagCount := range bagMap[bag] {
+		_, exists := validBags[bagCount]
+		if !exists {
+			validBags[bagCount] = 0
+		}
+		validBags[bagCount]++
+		validBags = crawlHolds(bagMap, bagCount.bag)
 	}
 	return validBags
 }
@@ -47,22 +65,27 @@ func main() {
 	}
 
 	bagMap := map[string][]string{}
+	countedBagMap := map[string][]BagCount{}
 	for _, bagRule := range input {
 		rule := strings.Split(bagRule, " contain ")
 		bag := strings.ReplaceAll(rule[0], " bags", "")
 		_, exists := bagMap[bag]
 		if !exists {
 			bagMap[bag] = []string{}
+			countedBagMap[bag] = []BagCount{}
 		}
 		bags := strings.Split(rule[1], ", ")
 		for _, storedBag := range bags {
 			parts := strings.Split(storedBag, " ")
 			color := parts[1] + " " + parts[2]
 			bagMap[bag] = append(bagMap[bag], color)
+			count, _ := strconv.Atoi(parts[0])
+			countedBagMap[bag] = append(countedBagMap[bag], BagCount{count, color})
 		}
 	}
 
-	bagCount := len(crawlBags(bagMap, "shiny gold", map[string]int{}))
+	canHoldShinyGold := len(crawlCanHold(bagMap, "shiny gold", map[string]int{}))
+	maxHold := len(crawlHolds(countedBagMap, "shiny gold", map[BagCount]int{}))
 
-	fmt.Printf("Day 07 - Handy Haversacks\nPart 1:\t%d\nPart 2:\t%d\n\n", bagCount, 0)
+	fmt.Printf("Day 07 - Handy Haversacks\nPart 1:\t%d\nPart 2:\t%d\n\n", canHoldShinyGold, maxHold)
 }
