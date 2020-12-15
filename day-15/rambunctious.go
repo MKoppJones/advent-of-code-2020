39fa21e22cbd0cb6db0ab1c23d14d8eafbd1c562
Michael Kopp-Jones <m.koppjones@gmail.com>
2020-12-15 08:26:45 +0000

    Day 15

--- day-15/rambunctious.go
+++ day-15/rambunctious.go
@@ -36,6 +36,52 @@ func main() {
 		println(err)
 		return
 	}
-	fmt.Println(input)
-	fmt.Printf("Day 15 - Rambunctious Recitation\nPart 1:\t%d\nPart 2:\t%d\n\n", 0, 0)
+	max := 2020
+	count := 0
+	numbers := map[int][]int{}
+	previousNum := 0
+
+	for index, num := range input {
+		numbers[num] = append(numbers[num], index+1)
+		count = index + 1
+		previousNum = num
+	}
+
+	for count < max {
+		if len(numbers[previousNum]) == 1 {
+			numbers[0] = append(numbers[0], count+1)
+			previousNum = 0
+		} else {
+			previousTimes := numbers[previousNum][len(numbers[previousNum])-2:]
+			previousNum = previousTimes[1] - previousTimes[0]
+			_, exists := numbers[previousNum]
+			if !exists {
+				numbers[previousNum] = []int{}
+			}
+			numbers[previousNum] = append(numbers[previousNum], count+1)
+		}
+		count++
+	}
+
+	part1 := previousNum
+
+	max = 30000000
+
+	for count < max {
+		if len(numbers[previousNum]) == 1 {
+			numbers[0] = append(numbers[0], count+1)
+			previousNum = 0
+		} else {
+			previousTimes := numbers[previousNum][len(numbers[previousNum])-2:]
+			previousNum = previousTimes[1] - previousTimes[0]
+			_, exists := numbers[previousNum]
+			if !exists {
+				numbers[previousNum] = []int{}
+			}
+			numbers[previousNum] = append(numbers[previousNum], count+1)
+		}
+		count++
+	}
+
+	fmt.Printf("Day 15 - Rambunctious Recitation\nPart 1:\t%d\nPart 2:\t%d\n\n", part1, previousNum)
 }
